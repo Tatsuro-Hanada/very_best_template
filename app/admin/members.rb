516d045bd8b5adf59a11f3b1cbe9ba5f9acb6773
Raghu Betina <raghu@firstdraft.com>
2019-03-09 06:25:18 +0000

    Whitelist columns in Member for ActiveAdmin

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Member do
+
+ permit_params :email, :password, :username, :cochair_flag
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
