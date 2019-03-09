40ea1f6003af83149912f9dc6b8b199fa63d0460
Raghu Betina <raghu@firstdraft.com>
2019-03-09 06:25:50 +0000

    Only review user can modify review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :current_member_must_be_review_member, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_member_must_be_review_member
+    review = Review.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_member == review.member
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @reviews = Review.all
 
