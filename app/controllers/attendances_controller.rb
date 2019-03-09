fb5b6238c336723d855b9d0c55532ab76d17ca78
Raghu Betina <raghu@firstdraft.com>
2019-03-09 06:25:50 +0000

    Only attendance user can modify attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,14 @@
 class AttendancesController < ApplicationController
+  before_action :current_member_must_be_attendance_member, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_member_must_be_attendance_member
+    attendance = Attendance.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_member == attendance.member
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @attendances = Attendance.all
 
