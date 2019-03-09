154bfb904998c029cd5612ab9c179ce5bb8ac373
Raghu Betina <raghu@firstdraft.com>
2019-03-09 06:25:01 +0000

    Direct association between events and interests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   # Direct associations
 
+  has_many   :interests,
+             :dependent => :destroy
+
   belongs_to :user,
              :class_name => "Member",
              :foreign_key => "member_id"
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,6 +1,8 @@
 class Interest < ApplicationRecord
   # Direct associations
 
+  belongs_to :event
+
   belongs_to :user,
              :class_name => "Member",
              :foreign_key => "member_id"
